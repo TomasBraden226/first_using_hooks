36dd0e5fec6451e1ca9fa8da3178f433664cc141
sharplight226 <sharplight226@gmail.com>
2023-03-20 21:39:01 +0200

    useEffect_training_second_param

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ export default function Counter() {
     // Declare state variables
     const [count, setCount] = useState(0);
     const [product, setProduct] = useState("Eggs");
+    const [detail, setDetail] = useState("Eggs is good for your health.");
     useEffect(() => {
     	console.log(`${product} will rule the world!`);
-    });
+    }, [product, detail]);
     return (
         <div>
-        Current {product}'s count: {count}
+        Current {product}'s count: {count}<br/>
+        {detail}
             <div>
                 <button onClick={() => setCount(count + 1)}>Add to cart</button>
                 <button onClick={() => setCount(count - 1)}>Remove from cart</button>
                 Change Product:{" "}
                 <input type="text" onChange={(e) => setProduct(e.target.value)} />
+                <input type="text" onChange={(e) => setDetail(e.target.value)} />
             </div>
         </div>
     );
